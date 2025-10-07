33a2a15c8ec9aaa95d2b69deabdb8023c019f31a
MJ-1112 <m22327803@gmail.com>
2025-10-07 09:06:07 +0530

    backend ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
+import multer from "multer";
+import {pdf} from 'pdf-parse';
+import officeParser from "officeparser";
+import fs from 'fs';
 
 dotenv.config();
 console.log("Loaded key:", process.env.GEMINI_API_KEY ? "✅ Found" : "❌ Not found");
@@ -8,6 +12,40 @@ console.log("Loaded key:", process.env.GEMINI_API_KEY ? "✅ Found" : "❌ Not f
 
 const app = express();
 app.use(express.json()); 
+const upload = multer({dest:"uploads/"});
+let text ="";
+app.post('/uploads',upload.single("file"), async(req,res)=>{
+    try {
+        const file = req.file;
+        if(!file){
+           return  res.send(402).json({message: "No file uploaded"});
+        }
+        // pdf file
+
+        if(file.mimetype == "application/pdf"){
+            const dataBuffer = await fs.readFileSync(file.path);
+            const pdfData = await pdf(dataBuffer);
+            text = pdfData.text;
+            res.send(202).json({message: "pdf uploaded and read successfully"});
+        }
+
+        //ppt file
+
+        else if (file.mimetype === "application/vnd.openxmlformats-officedocument.presentationml.presentation") {
+             const pptText = await officeParser.parseOfficeAsync(file.path);
+             text = pptText;
+        }
+        else{
+            res.send(501).json({error:"File type not compatible"});
+        }
+        await fs.unlinkSync(file.path);
+        
+    } catch (error) {
+        return res.send(400).json({error});
+        
+    }
+
+})
 
 
 const ai = new GoogleGenAI({});
@@ -36,6 +74,26 @@ main();
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+app.post('/summarize',async(req,res)=>{
+    const prompt = `Summarize this for me in simple words as I am a student and you are a proffesional teacher teaching me , and make bullet go to points for last minute prep for the text : ${text}`;
+    async function main() {
+      const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: prompt,
+  });
+
+  res.status(200).json({ answer: response.text });
+  
+
+
+}
+
+main();
+})
+
+
+
 
 app.listen(3000, () => {
   console.log("✅ App is running on http://localhost:3000");
